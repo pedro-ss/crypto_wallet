21327c184f1a237bbef51d06692a27904d2a5c41
pedross <pedross.tool@gmail.com>
2024-07-30 23:47:16 -0300

    projeto modulo 01

---
+++ README.md
@@ -0,0 +1,7 @@
+## PROJETO DE EXEMPLO, BASEADO EM PROJETO DO MODULO-01, APRESENTADO EM BOOTCAMP DA DIO
+```
+https://github.com/digitalinnovationone/formacao-blockchain-dio.git
+```
+
+### DEPENDENCIAS
+- node 
---
+++ src/createWallet.js
@@ -0,0 +1,35 @@
+//importando dependencias
+const bip32 = require('bip32')
+const bip39 = require('bip39')
+const bitcoin = require('bitcoinjs-lib')
+
+//definindo rede como testnet
+//bitcoin - rede principal - mainnet
+//testnet - rede de teste - testnet
+const network = bitcoin.networks.testnet
+
+//derivação de carteiras HD
+const path = `m/49'/1'/0'/0` 
+
+//criando o mnemonic para a seed (palavras de senha)
+let mnemonic = bip39.generateMnemonic()
+const seed = bip39.mnemonicToSeedSync(mnemonic)
+
+//criando a raiz da cartiera HD
+let root = bip32.fromSeed(seed, network)
+
+//criando uma conta - par pvt-pub keys (chaves privada e pública)
+let account = root.derivePath(path)
+let node = account.derive(0).derive(0)
+
+//criando endereço
+let btcAddress = bitcoin.payments.p2pkh({
+    pubkey: node.publicKey,
+    network: network,
+}).address
+
+// logando informações
+console.log("Carteira gerada")
+console.log("Endereço: ", btcAddress)
+console.log("Chave privada:", node.toWIF())
+console.log("Seed:", mnemonic)
